4d3cc9db3fed18d264fe230db82c50f0e82e93da
Erik1007 <erik.hartman1007@gmail.com>
2023-02-11 19:28:05 +0000

    editing and debugging src and notebook code

--- src/machine_learning/predictive_analysis.py
+++ src/machine_learning/predictive_analysis.py
@@ -4,7 +4,7 @@ import pandas as pd
 import plotly.express as px
 from tensorflow.keras.models import load_model
 from PIL import Image
-from src.data_management import load_pkl_file
+from src.data_management import load_pickle_file
 
 
 def resize_input_image(img, version):
@@ -12,7 +12,7 @@ def resize_input_image(img, version):
     Resize image to average image size
     """
     image_shape = load_pickle_file(
-        file_name=f"outputs/{version}/image_shape.pk1")
+        file_name=f"outputs/{version}/image_shape.pkl")
     img_resized = img.resize((image_shape[1], image_shape[0]), Image.ANTIALIAS)
     return np.expand_dims(img_resized, axis=0)/255
 
